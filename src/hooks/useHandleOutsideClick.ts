eb6e2d50225f7c4a369148f7da9c7de7e75e4083
Toshio Minei <sminei10@gmail.com>
2023-05-29 17:06:35 -0500

    chore: implemented useHandleOutsideClick hook

---
+++ src/hooks/useHandleOutsideClick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+function useHandleOutsideClick(callback?: () => void) {
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClick(event: Event) {
+      if (container.current && !container.current.contains(event.target as Node)) {
+        callback && callback();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [callback]);
+
+  return container;
+}
+
+export default useHandleOutsideClick;
