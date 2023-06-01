b74333cf76c39640960628ac08b3874806b7f4a5
Toshio Minei <sminei10@gmail.com>
2023-06-01 18:40:43 -0500

    chore: created AddButton component

---
+++ src/components/modules/AddButton.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { FC, useState } from "react";
+import Icons from "@/core/Icons";
+import Modal from "@/modules/Modal";
+import Typography from "../core/Typography";
+import Input from "@/core/Input";
+import Button from "@/core/Button";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormType = {
+  value: string;
+};
+
+type AddButtonProps = {
+  title: string;
+  onAdd: (value: string) => void;
+};
+
+const AddButton: FC<AddButtonProps> = ({ title, onAdd }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormType>();
+
+  const toggleOpen = () => setIsOpen((o) => !o);
+
+  const handleClose = () => {
+    toggleOpen();
+    reset();
+  };
+
+  const onSubmit: SubmitHandler<FormType> = ({ value }) => {
+    toggleOpen();
+    onAdd(value);
+  };
+
+  return (
+    <>
+      <button className="group" onClick={handleClose}>
+        <Icons.Add className="text-primary group-hover:text-green-600" />
+      </button>
+      <Modal open={isOpen} onClose={toggleOpen}>
+        <form className="p-4" onSubmit={handleSubmit(onSubmit)}>
+          <Typography className="mb-4 font-semibold" size="lg">
+            {title}
+          </Typography>
+          <Input
+            variant="standard"
+            {...register("value", { required: true })}
+          />
+          {errors.value && (
+            <Typography className="mt-1 text-error" size="xs">
+              Este campo es obligatorio
+            </Typography>
+          )}
+          <div className="mt-6 flex justify-end">
+            <Button variant="outline" className="mr-2" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button type="submit">Guardar</Button>
+          </div>
+        </form>
+      </Modal>
+    </>
+  );
+};
+
+export default AddButton;
