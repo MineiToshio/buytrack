42c7ec53e163380aaef0f3ebb4ce3d7aec8523cc
Toshio Minei <sminei10@gmail.com>
2023-06-01 18:36:39 -0500

    chore: created post and get request helpers

---
+++ src/helpers/request.ts
@@ -0,0 +1,27 @@
+import { HTTP_METHOD } from "next/dist/server/web/http";
+
+const getBody = (verb: HTTP_METHOD, data?: any) => ({
+  method: verb,
+  body: JSON.stringify(data),
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+const request = async <T>(
+  url: string,
+  options?: RequestInit
+): Promise<T | undefined> => {
+  try {
+    const res = await fetch(url, options);
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const post = <T>(url: string, data?: any) =>
+  request<T>(url, getBody("POST", data));
+
+export const get = <T>(url: string) => request<T>(url);
