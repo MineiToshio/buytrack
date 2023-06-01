9e4a6e318c7988aa6837374e08ed79e2d3f8cc96
Toshio Minei <sminei10@gmail.com>
2023-06-01 18:35:48 -0500

    chore: add react query configuration

--- src/components/modules/Providers.tsx
+++ src/components/modules/Providers.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
-const Providers: FC<ProvidersProps> = ({ children }) => (
-  <SessionProvider>{children}</SessionProvider>
-);
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  const [client] = useState(
+    new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
+  );
+
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={client}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
+};
 
 export default Providers;
