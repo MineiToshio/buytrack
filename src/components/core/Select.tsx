255f7ef8b75f97893442a910468e9ed0249750f0
Toshio Minei <sminei10@gmail.com>
2023-06-01 18:32:11 -0500

    chore: enable undefined options in select component

--- src/components/core/Select.tsx
+++ src/components/core/Select.tsx
@@ -40,7 +40,7 @@ export type Option = {
 type SelectProps = VariantProps<typeof selectVariants> & {
   className?: string;
   placeholder?: string;
-  options: Array<Option>;
+  options?: Array<Option>;
   value: OptionValue;
   onChange: (value: OptionValue) => void;
 };
@@ -81,25 +81,35 @@ const Select: FC<SelectProps> = ({
       )}
       <div
         className={cn(
-          "top-13 absolute left-[-1px] z-10 flex w-[calc(100%+2px)] flex-col overflow-y-auto rounded-b-md border border-t-0 border-solid bg-white shadow-md transition",
+          "top-13 absolute left-[-1px] z-10 flex w-[calc(100%+2px)] flex-col overflow-y-auto rounded-b-md border border-t-0 border-solid bg-white shadow-md transition max-h-40",
           {
             hidden: !isOpen,
           }
         )}
       >
-        {options.map((o) => {
-          const key =
-            typeof o.value === "boolean" ? (o.value ? 1 : 0) : o.value;
-          return (
-            <span
-              className="w-full select-none p-2 hover:bg-primary hover:text-white"
-              key={key}
-              onClick={() => onChange(o.value)}
-            >
-              {o.label}
-            </span>
-          );
-        })}
+        {options ? (
+          <>
+            {options.map((o) => {
+              const key =
+                typeof o.value === "boolean" ? (o.value ? 1 : 0) : o.value;
+              return (
+                <span
+                  className="group w-full p-2 hover:bg-primary"
+                  key={key}
+                  onClick={() => onChange(o.value)}
+                >
+                  <Typography className="select-none group-hover:text-white">
+                    {o.label}
+                  </Typography>
+                </span>
+              );
+            })}
+          </>
+        ) : (
+          <Typography className="cursor-default select-none p-2 text-muted">
+            No hay opciones
+          </Typography>
+        )}
       </div>
     </div>
   );
