dec0c7cebecb6804e4b28aa2e4b6fe9f93a2cb38
Toshio Minei <sminei10@gmail.com>
2023-06-01 18:41:00 -0500

    chore: created Icons component

---
+++ src/components/core/Icons.tsx
@@ -0,0 +1,27 @@
+import {
+  AlignJustify,
+  Facebook,
+  Globe,
+  Instagram,
+  Loader2,
+  MessageCircle,
+  ChevronDownSquare,
+  PlusCircle,
+  X,
+  Globe2,
+} from "lucide-react";
+
+export const Icons = {
+  Loader: Loader2,
+  Text: AlignJustify,
+  Facebook,
+  Instagram,
+  Message: MessageCircle,
+  Web: Globe,
+  ChevronSquareDown: ChevronDownSquare,
+  Add: PlusCircle,
+  Cancel: X,
+  Globe: Globe2,
+};
+
+export default Icons;
