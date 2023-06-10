dcbaa1ffe4f28266aa7d7d1cf236b82dff0df07a
Toshio Minei <sminei10@gmail.com>
2023-06-09 20:38:09 -0500

    chore: add secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
           light: "#22c55e",
           DEFAULT: "#22c55e",
         },
+        secondary: {
+          light: "#1e3a8a",
+          DEFAULT: "#1e3a8a",
+        },
         muted: {
           light: "#a1a1aa",
           DEFAULT: "#a1a1aa",
