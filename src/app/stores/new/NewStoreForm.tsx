881517ba77a9cab912c55d40f0d3a58b6270f03c
Toshio Minei <sminei10@gmail.com>
2023-06-13 13:22:08 -0500

    feat: return to stores page when creating a store

--- src/app/stores/new/NewStoreForm.tsx
+++ src/app/stores/new/NewStoreForm.tsx
@@ -1,31 +1,43 @@
 "use client";
 
-import { FC } from "react";
-import { SubmitHandler } from "react-hook-form";
-import StoreForm, { StoreFormType } from "../StoreForm";
-import { post } from "@/helpers/request";
 import { CREATE_STORE } from "@/helpers/apiUrls";
-import { useMutation } from "@tanstack/react-query";
-import { Store } from "@prisma/client";
-import slugify from "slugify";
+import { post } from "@/helpers/request";
 import { generateId } from "@/helpers/utils";
+import { Store } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { FC, useState } from "react";
+import { SubmitHandler } from "react-hook-form";
+import slugify from "slugify";
+import StoreForm, { StoreFormType } from "../StoreForm";
 
-const createStore = (data: StoreFormType) => {
+const createStore = async (data: StoreFormType) => {
   const randomString = generateId(5);
   const url = slugify(`${data.name}-${randomString}`);
   return post<Store>(CREATE_STORE, { ...data, url });
 };
 
 const NewStoreForm: FC = () => {
+  const router = useRouter();
+  const [isPending, setIsPending] = useState<boolean>(false);
+
   const { isLoading, mutate } = useMutation({
     mutationFn: (data: StoreFormType) => createStore(data),
+    onSuccess: () => {
+      setIsPending(true);
+      router.push("/stores");
+      // This refresh is to force the await in /stores page
+      router.refresh();
+    },
   });
 
   const submitHandler: SubmitHandler<StoreFormType> = (data) => {
     mutate(data);
   };
 
-  return <StoreForm onSubmit={submitHandler} isLoading={isLoading} />;
+  return (
+    <StoreForm onSubmit={submitHandler} isLoading={isLoading || isPending} />
+  );
 };
 
 export default NewStoreForm;
