61c9e8d4d01ad2b9d9f7e2138cc2acc0c0d3eaa8
Toshio Minei <sminei10@gmail.com>
2023-06-16 01:47:47 -0500

    chore: added search input in select component

--- src/components/core/Select.tsx
+++ src/components/core/Select.tsx
@@ -4,8 +4,9 @@ import Chip from "@/core/Chip";
 import useHandleOutsideClick from "@/hooks/useHandleOutsideClick";
 import { cn } from "@/styles/utils";
 import { VariantProps, cva } from "class-variance-authority";
-import { FC, useMemo, useState } from "react";
+import { FC, useMemo, useRef, useState } from "react";
 import Typography from "./Typography";
+import Input from "./Input";
 
 const selectVariants = cva("relative w-full cursor-pointer rounded-md p-2", {
   variants: {
@@ -59,8 +60,10 @@ type MultipleOption = {
 type SelectProps = VariantProps<typeof selectVariants> & {
   className?: string;
   placeholder?: string;
+  searchPlaceholder?: string;
   options?: Array<Option>;
   readOnly?: boolean;
+  allowSearch?: boolean;
 } & (SingleOption | MultipleOption);
 
 const Select: FC<SelectProps> = ({
@@ -69,12 +72,27 @@ const Select: FC<SelectProps> = ({
   onChange,
   className,
   placeholder,
+  searchPlaceholder,
   variant,
   multiple,
   readOnly,
+  allowSearch,
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const selectRef = useHandleOutsideClick(() => setIsOpen(false));
+  const [search, setSearch] = useState<string>("");
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setSearch("");
+  };
+
+  const selectRef = useHandleOutsideClick(handleClose);
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const filteredOptions = useMemo(() => {
+    if (search.trim() === "") return options;
+    return options?.filter((o) => o.label.includes(search.trim()));
+  }, [options, search]);
 
   const selectedOption = useMemo(() => {
     if (options && value != null) {
@@ -99,6 +117,7 @@ const Select: FC<SelectProps> = ({
     } else {
       onChange(selectedValue);
     }
+    setSearch("");
   };
 
   const handleDelete = (selectedValue: OptionValue) => {
@@ -109,7 +128,10 @@ const Select: FC<SelectProps> = ({
     }
   };
 
-  const toggleOpen = () => setIsOpen((o) => !o);
+  const toggleOpen = () => {
+    setIsOpen((o) => !o);
+    setTimeout(() => searchRef?.current?.focus(), 100);
+  };
 
   return (
     <div
@@ -123,33 +145,59 @@ const Select: FC<SelectProps> = ({
       onClick={toggleOpen}
       ref={selectRef}
     >
-      {selectedOption && selectedOption.length > 0 ? (
-        <>
-          {multiple ? (
-            <div className="flex">
-              {selectedOption.map((so) => {
-                const key =
-                  typeof so.value === "boolean" ? (so.value ? 1 : 0) : so.value;
-                return (
-                  <Chip
-                    className="mr-2"
-                    label={so.label}
-                    key={key}
-                    readOnly={readOnly}
-                    onDelete={() => handleDelete(so.value)}
-                  />
-                );
-              })}
-            </div>
-          ) : (
-            <Typography>{selectedOption[0].label}</Typography>
-          )}
-        </>
-      ) : (
-        <Typography color="muted" className="select-none">
-          {placeholder}
-        </Typography>
-      )}
+      <div className="flex w-full flex-wrap">
+        {selectedOption && selectedOption.length > 0 ? (
+          <>
+            {multiple ? (
+              <div className="flex">
+                {selectedOption.map((so) => {
+                  const key =
+                    typeof so.value === "boolean"
+                      ? so.value
+                        ? 1
+                        : 0
+                      : so.value;
+                  return (
+                    <Chip
+                      className="mr-2"
+                      label={so.label}
+                      key={key}
+                      readOnly={readOnly}
+                      onDelete={() => handleDelete(so.value)}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <>
+                {!(allowSearch && isOpen) && (
+                  <Typography>{selectedOption[0].label}</Typography>
+                )}
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            {!(allowSearch && isOpen) && (
+              <Typography color="muted" className="select-none">
+                {placeholder}
+              </Typography>
+            )}
+          </>
+        )}
+        <Input
+          variant="unstyled"
+          ref={searchRef}
+          value={search}
+          placeholder={searchPlaceholder}
+          onChange={(e) => setSearch(e.target.value)}
+          onClick={(e) => e.stopPropagation()}
+          className={cn("min-w-[100px] bg-transparent outline-0", {
+            hidden: !(allowSearch && isOpen),
+            "mt-2": multiple && value && value?.length > 0,
+          })}
+        />
+      </div>
       <div
         className={cn(
           "top-13 absolute left-[-1px] z-10 flex max-h-40 w-[calc(100%+2px)] flex-col overflow-y-auto rounded-b-md border border-t-0 border-solid bg-white shadow-md transition",
@@ -158,9 +206,9 @@ const Select: FC<SelectProps> = ({
           },
         )}
       >
-        {options && options.length > 0 ? (
+        {filteredOptions && filteredOptions.length > 0 ? (
           <>
-            {options.map((o) => {
+            {filteredOptions.map((o) => {
               const key =
                 typeof o.value === "boolean" ? (o.value ? 1 : 0) : o.value;
               return (
