3a8d3bd5c78be070442143ddb4a434c9abbe7673
Toshio Minei <sminei10@gmail.com>
2023-07-05 19:00:17 -0500

    chore: implemented formatDate function

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,5 @@
+import { Locale } from "@/types/types";
+
 export const generateId = (length: number) => {
   let result = "";
   const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -8,4 +10,21 @@ export const generateId = (length: number) => {
     counter += 1;
   }
   return result;
+};
+
+export const formatDate = (date: Date, lang: Locale = "es") => {
+  const day = date.getDate().toString();
+  const month = (date.getMonth() + 1).toString();
+  const year = date.getFullYear().toString();
+
+  const formattedDay = `${day.length === 1 ? "0" : ""}${day}`;
+  const formattedMonth = `${month.length === 1 ? "0" : ""}${month}`;
+
+  switch (lang) {
+    case "es":
+      return `${formattedDay}/${formattedMonth}/${year}`;
+    case "en":
+    default:
+      return `${formattedMonth}/${formattedDay}/${year}`;
+  }
 };
