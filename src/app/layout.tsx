82f67fcdfb86478175268f72ebd4c794c0a52dc3
Toshio Minei <sminei10@gmail.com>
2023-07-17 18:43:04 -0500

    feat: show loader bar when changing pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
-import Providers from "@/modules/Providers";
+import Footer from "@/modules/Footer";
 import Navbar from "@/modules/Navbar";
+import Providers from "@/modules/Providers";
 import { regularFont } from "@/styles/fonts";
 import "@/styles/globals.css";
 import { cn } from "@/styles/utils";
-import Footer from "@/modules/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="es">
       <body className={cn("pt-20", regularFont.className)}>
+        <NextTopLoader />
         <Providers>
           {/* @ts-expect-error */}
           <Navbar />
