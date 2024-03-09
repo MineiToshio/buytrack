19ad9774dc14bd3701a84fae8e4ee9b61d120538
Toshio Minei <sminei10@gmail.com>
2024-03-09 01:28:14 -0500

    chore: add seed for productType, country and productCountry

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,9 +29,52 @@ const addCurrency = async () => {
   });
 };
 
+const addProductType = async () => {
+  await prisma.productType.createMany({
+    data: [
+      { name: "Manga" },
+      { name: "Funko" },
+      { name: "Cartas" },
+      { name: "Cuidado Mangas" },
+      { name: "Comics" },
+    ],
+  });
+};
+
+const addCountry = async () => {
+  await prisma.country.createMany({
+    data: [
+      { name: "USA" },
+      { name: "Perú" },
+      { name: "México" },
+      { name: "Colombia" },
+      { name: "Argentina" },
+      { name: "Chile" },
+      { name: "España" },
+    ],
+  });
+};
+
+const addProductsCountry = async () => {
+  await prisma.productsCountry.createMany({
+    data: [
+      { name: "USA" },
+      { name: "Perú" },
+      { name: "México" },
+      { name: "Colombia" },
+      { name: "Argentina" },
+      { name: "Chile" },
+      { name: "España" },
+    ],
+  });
+};
+
 const main = async () => {
   try {
     await addCurrency();
+    await addProductType();
+    await addCountry();
+    await addProductsCountry();
     await prisma.$disconnect();
   } catch (e) {
     console.error(e);
